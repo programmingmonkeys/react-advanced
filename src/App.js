2676b100f57d89a3b035c20381c92aeef2d83a3f
codingChewie <codingChewie@gmail.com>
2020-12-29 20:14:01 -0600

    context in class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css'
-// import Movie from './hoc/Movie'
-// import Counter from './hooks/Counter'
-import Users from './hooks/Users'
+import React, { Component } from 'react'
+import MoviePage from './context/MoviePage'
+import UserContext from './context/userContext'
 
-function App() {
-  // return <Movie id={1} />
-  // return <Counter />
-  return <Users />
+class App extends Component {
+  state = { currentUser: { name: 'Foo' } }
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+    )
+  }
 }
 
 export default App
---
+++ src/HookApp.js
@@ -0,0 +1,12 @@
+import './App.css'
+// import Movie from './hoc/Movie'
+// import Counter from './hooks/Counter'
+import Users from './hooks/Users'
+
+function App() {
+  // return <Movie id={1} />
+  // return <Counter />
+  return <Users />
+}
+
+export default App
